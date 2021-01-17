888f96daaf6816ab1af50f8e111f89137de31a3d
Emmanuel Afuadajo <aquilaafuadajo@gmail.com>
2021-01-17 06:17:04 +0100

    added nomination logic and transition effects

--- src/App.js
+++ src/App.js
@@ -1,25 +1,170 @@
 import React, { useState, useEffect } from "react";
 import "./assets/scss/App.scss";
 import Header from "./components/header/Header";
+import MovieCard from "./components/movie-card/MovieCard";
+import NominationCard from "./components/nomination-card/NominationCard";
+import Search from './components/search/Search'
 
 function App() {
-  const [wordToSearch, setSwordToSearch] = useState("bridge");
-  const [isMobile, toggleIsMobile] = useState(false);
+  const [mobileSidebar, toggleMobileSidebar] = useState(false);
+  const [movies, setMovies] = useState([]);
+  const [nominations, setNominations] = useState([]);
+  const [wordToSearch, setWordToSearch] = useState("bridge");
+  const [limit, setLimit] = useState(5);
+
+  useEffect(() => {
+    fetch(
+      `https://www.omdbapi.com/?s=${wordToSearch}&page=1-10&apikey=dee16f2c`
+    )
+      .then((res) => res.json())
+      .then((data) => setMovies(data.Search || []));
+
+    const storedNominations = localStorage.getItem("shoppiesNominations");
+
+    // fetch nominations from local storage
+    storedNominations !== null
+      ? setNominations(JSON.parse(storedNominations))
+      : localStorage.setItem(
+          "shoppiesNominations",
+          JSON.stringify(nominations)
+        );
+
+    // fetch limit from local storage
+    const storedLimit = localStorage.getItem("nominationLimit");
+
+    storedLimit === null
+      ? localStorage.setItem("nominationLimit", 5)
+      : setLimit(Number(storedLimit));
+
+  }, []);
 
   const handleSearch = (e) => {
-    e.preventDefault()
+    e.preventDefault();
 
-    console.log(e.target.value)
-  }
+    setWordToSearch(e.target.value);
+
+    if (e.target.value.length >= 3) {
+      fetch(
+        `https://www.omdbapi.com/?s=${e.target.value}&page=1-10&apikey=dee16f2c`
+      )
+        .then((res) => res.json())
+        .then((data) => setMovies(data.Search || []));
+    } else {
+      setMovies([]);
+    }
+  };
+
+  const nominateMovie = (movie) => {
+    const newMoviesArray = [movie, ...nominations];
+    setNominations(newMoviesArray);
+
+    // add movie to loacalstorage
+    localStorage.setItem("shoppiesNominations", JSON.stringify(newMoviesArray));
+
+    // update limit
+    setLimit(limit - 1);
+
+    // update local storage limit
+    localStorage.setItem("nominationLimit", limit - 1);
+
+    nominations.length === 5 &&
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+  };
+
+  const undoNomination = (movie) => {
+    const currentNominations = nominations.filter(
+      (item) => item.imdbID !== movie.imdbID
+    );
+
+    // update local storage
+    setNominations(currentNominations);
+    localStorage.setItem(
+      "shoppiesNominations",
+      JSON.stringify(currentNominations)
+    );
+
+    // update limit
+    setLimit(limit + 1);
+
+    // update local storage limit
+    localStorage.setItem("nominationLimit", limit + 1);
+  };
 
   return (
-    <div className='App' id={isMobile ? "no-scroll" : ""}>
+    <div className='App' id={mobileSidebar ? "no-scroll" : ""}>
       <Header
-        isMobile={isMobile}
-        toggleIsMobile={toggleIsMobile}
+        mobileSidebar={mobileSidebar}
+        toggleMobileSidebar={toggleMobileSidebar}
+        limit={limit}
         wordToSearch={wordToSearch}
         handleSearch={handleSearch}
       />
+      <Search className='mobile-search' wordToSearch={wordToSearch} handleSearch={handleSearch} />
+      <div className='main-wrapper'>
+        <div className='main-wrapper__inner'>
+          <main>
+            {nominations.length === 5 && (
+              <div className='banner' role='banner'>
+                <i className='fas fa-exclamation-circle'></i>
+                <p>
+                  You've used up your nominations, Thanks!
+                </p>
+              </div>
+            )}
+            {movies.length > 0 && (
+              <div className='stats'>
+                <p>
+                  Showing <span>{movies.length}</span> results for keyword{" "}
+                  <span>'{wordToSearch}'</span>
+                </p>
+              </div>
+            )}
+
+            <div className='movies__listing'>
+              {movies.map((movie) => (
+                <MovieCard
+                  key={movie.imdbID}
+                  movieData={movie}
+                  nominateMovie={nominateMovie}
+                  nominations={nominations}
+                  limit={limit}
+                />
+              ))}
+            </div>
+          </main>
+
+          <aside
+            className={`nominations ${
+              mobileSidebar ? "show-sidebar" : "hide-sidebar"
+            }`}
+          >
+            <h3 className='nominations__heading'>Your Nominations</h3>
+            {/* <p className='nominations__limit'>
+              You have <strong>{limit} Nominations</strong> Remaining
+            </p> */}
+
+            {nominations.length === 0 && (
+              <div className='nominations__empty'>
+                <i className='fas fa-ghost'></i>
+                <p>Empty!</p>
+              </div>
+            )}
+
+            <div className='nominations__listing'>
+              {nominations.map((movie) => (
+                <NominationCard
+                  key={movie.imdbID}
+                  movieData={movie}
+                  undoNomination={undoNomination}
+                />
+              ))}
+            </div>
+          </aside>
+        </div>
+      </div>
     </div>
   );
 }
